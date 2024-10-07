042714280222ea0ed9d6d766ead168da63fd92e9
Jerry Nixon <jerry.nixon@microsoft.com>
2024-10-07 16:32:41 -0600

    Temporary error removed.

--- Logic/Class1.cs
+++ Logic/Class1.cs
@@ -9,7 +9,7 @@
 
         public async Task<string> GetFunctionSample(HttpClient httpClient)
         {
-            return await httpClient.GetStringAsync("/Function1");
+            return "";// await httpClient.GetStringAsync("/Function1");
         }
 
         public async Task<string> GetDatabaseSample(HttpClient httpClient)
