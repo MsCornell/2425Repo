4dbd4f053017193a820a595239cffde800f4d97f
Jerry Nixon <jerry.nixon@microsoft.com>
2024-10-14 14:28:01 -0600

    updated vscode launch files

--- database/Tables/Player.sql
+++ database/Tables/Player.sql
@@ -1,8 +1,17 @@
 CREATE TABLE Player(
-	Id int PRIMARY KEY,
-	OAuthId varchar(150) NOT NULL,
-	Name varchar(150),
-	Created datetime DEFAULT GETDATE(),
-	_username varchar(150),
-	_password varchar(150)
-) 
+    Id int PRIMARY KEY,
+    OAuthId varchar(150) NOT NULL,
+    Name varchar(150),
+    Created datetime DEFAULT GETDATE(),
+    _username varchar(150),
+    _password varchar(150)
+);
+
+/*
+GO
+
+ALTER TABLE Player
+	ALTER COLUMN _password 
+		ADD MASKED 
+			WITH (FUNCTION = 'default()');
+*/
